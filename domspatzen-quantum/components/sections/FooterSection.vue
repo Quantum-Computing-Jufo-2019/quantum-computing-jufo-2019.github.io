e5babb62049b6a3cd7aa817fc2667bd4383a8908
Jonathan Treffler <mail@jonathan-treffler.de>
2020-07-19 12:13:34 +0200

    Update FooterSection.vue

--- domspatzen-quantum/components/sections/FooterSection.vue
+++ domspatzen-quantum/components/sections/FooterSection.vue
@@ -1,6 +1,12 @@
 <template>
 	<div class="section" id="footer_section">
-		<p class="footer_text">©Jakov Wallbrecher (14), Paul Schappert (15), Jonathan Treffler (16) , 2019</p>
+		<p class="footer_text footer_text_copyright">©Jakov Wallbrecher (14), Paul Schappert (15), Jonathan Treffler (16) , 2019</p>
+    <div class="footer_text_phone_copyright">
+    	<span>Jakov Wallbrecher (14)</span>
+			<span>Paul Schappert (15)</span>
+			<span>Jonathan Treffler (16)</span>
+			<span>2019</span>
+		</div>
 		<p class="footer_text">mail@domspatzen-quantum.de</p>
 	</div>
 </template>
@@ -23,5 +29,16 @@
 		padding-left: 6px;
 		padding-right: 6px;
 		font-size: x-large;
+	}
+	html:not(.phone) .footer_text_phone_copyright {
+		display: none;
+	}
+	html.phone .footer_text_phone_copyright {
+		display: flex;
+		flex-flow: column;
+    flex-wrap: nowrap;
+	}
+	html.phone .footer_text_copyright {
+		display: none;
 	}
 </style>
