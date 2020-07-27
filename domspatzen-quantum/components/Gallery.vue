28b94e8a105244bf57201f7ff691a95aec870e02
Jonathan Treffler <mail@jonathan-treffler.de>
2020-07-27 17:24:37 +0200

    cleaned up

--- domspatzen-quantum/components/Gallery.vue
+++ domspatzen-quantum/components/Gallery.vue
@@ -155,12 +155,6 @@ export default {
 			}
 
 			if (new_current_picture) {
-				const offset = 0;
-
-				/*$(picture_container).scrollTo(new_current_picture, 500, {
-					offset,
-				});*/
-
 				this.scrollToElement({
 					container: picture_container,
 					element: new_current_picture,
@@ -191,20 +185,16 @@ export default {
 	    var _x = 0;
 	    var _y = 0;
 	    while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
-	        _x += el.offsetLeft /*- el.scrollLeft*/;
-	        _y += el.offsetTop /*- el.scrollTop*/;
+	        _x += el.offsetLeft;
+	        _y += el.offsetTop;
 	        el = el.offsetParent;
 	    }
 	    return { top: _y, left: _x };
 		},
 		scrollToElement(options) {
-			// options: container, element, duration
 			let scrollLeftStart = options.container.scrollLeft;
 			let containerOffsetLeft = this.getOffset(options.container).left;
 			let scrollLeftEnd = options.element.offsetLeft - containerOffsetLeft;
-
-			console.log(options);
-			console.log(scrollLeftStart, scrollLeftEnd, containerOffsetLeft);
 
 			let start;
 
@@ -218,14 +208,9 @@ export default {
 				}
 
 				const elapsed = timestamp - start;
-
 				const progress = Math.min(elapsed/options.duration, 1);
 
-				console.log(elapsed, progress);
-
 				let currentLeft = scrollLeftStart + ((scrollLeftEnd - scrollLeftStart)*easeInOutCubic(progress));
-
-				console.log(currentLeft);
 
 				options.container.scrollLeft = currentLeft;
 
