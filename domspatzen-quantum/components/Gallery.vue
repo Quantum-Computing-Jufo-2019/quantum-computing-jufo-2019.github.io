0fe709fb8077c57b1df89ed91022b4a0539bce34
Jonathan Treffler <mail@jonathan-treffler.de>
2020-07-27 21:47:54 +0200

    removed jquery from image zooming

--- domspatzen-quantum/components/Gallery.vue
+++ domspatzen-quantum/components/Gallery.vue
@@ -30,105 +30,137 @@ export default {
 	mounted() {
 		this.$on('zoomPicture', function(id) {
 			let picture;
-			// const picture_container = $(this.$el).find('.picture_container')[0]
 			const picture_container = this.$el.querySelector('.picture_container');
 			console.log(picture_container);
-			const pictures = picture_container.childNodes;
+			const pictures = picture_container.children;
 
 			for (let i = 0; i < pictures.length; i++) {
+				console.log(pictures[i]);
 				if (pictures[i].getAttribute('data-id') == id) {
 					picture = pictures[i];
 				}
 			}
 
-			$(this.$el).addClass('zoom');
+			this.$el.classList.add('zoom');
 
-			const pos = $(picture).offset();
-			const height = $(picture).height();
-			const width = $(picture).width();
+			const pos = picture.getBoundingClientRect();
+			const height = pos.height;
+			const width = pos.width;
 
-			const zoomedImage = $(this.$el).find('.zoomed_image');
-			zoomedImage.show();
+			console.log(height, width);
 
-			this.currentWebpSrc = $(picture).attr('data-webp-src');
-			this.currentJpgSrc = $(picture).attr('data-jpg-src');
+			let zoomedImage = this.$el.querySelector('.zoomed_image');
+			zoomedImage.style.display = "";
+
+			this.currentWebpSrc = picture.getAttribute('data-webp-src');
+			this.currentJpgSrc = picture.getAttribute('data-jpg-src');
 
 			function setToOrgiginalPos(pos, width, height) {
 				return new Promise(function(resolve, reject) {
+					console.log("setToOrgiginalPos");
 					console.log(pos);
-					zoomedImage.offset(pos);
-					zoomedImage.css('width', width);
-					zoomedImage.css('height', height);
-					zoomedImage.css('max-width', width);
-					zoomedImage.css('max-height', height);
-					resolve();
+					zoomedImage.style.top = pos.top + "px";
+					zoomedImage.style.left = pos.left + "px";
+					zoomedImage.style.width = width + "px";
+					zoomedImage.style.height = height + "px";
+					zoomedImage.style.maxWidth = width + "px";
+					zoomedImage.style.maxHeight = height + "px";
+
+					setTimeout(function() {
+						resolve();
+					}, 1000);
 				});
 			}
 
 			function activateBlurBackground(thisEl) {
 				return new Promise(function(resolve, reject) {
-					const zoomBlurBackground = $(thisEl).find('.zoom_blur_background');
-					zoomBlurBackground.show();
-					zoomBlurBackground.animate({
-						opacity: 0.8,
-					});
+					console.log("activateBlurBackground");
+					const zoomBlurBackground = thisEl.querySelector('.zoom_blur_background');
+					zoomBlurBackground.style.display = '';
+					zoomBlurBackground.style.opacity = 0.8;
+
+					setTimeout(function() {
+						resolve();
+					}, 1000);
 				});
 			}
 
 			function centerImagePX() {
 				return new Promise(function(resolve, reject) {
-					zoomedImage.animate({
-						top: ($('html').height() - zoomedImage.height()) / 2 + 'px',
-						left: ($('html').width() - zoomedImage.width()) / 2 + 'px',
-					}, function() {
+					console.log("centerImagePX");
+					let htmlBounds = document.getElementsByTagName("html")[0].getBoundingClientRect();
+					let zoomedImageBounds = zoomedImage.getBoundingClientRect();
+
+					zoomedImage.classList.add("topLeftTransition");
+					zoomedImage.style.top = (htmlBounds.height - zoomedImageBounds.height) / 2 + 'px';
+					zoomedImage.style.left = (htmlBounds.width - zoomedImageBounds.width) / 2 + 'px';
+
+					setTimeout(function() {
+						zoomedImage.classList.remove("topLeftTransition");
 						resolve();
-					});
+					}, 1000);
 				});
 			}
 
 			function centerImagePC() {
 				return new Promise(function(resolve, reject) {
-					zoomedImage.css({
-						top: '50%',
-						left: '50%',
-						transform: 'translate(-50%, -50%)',
-					});
-					resolve();
+					console.log("centerImagePC");
+					zoomedImage.style.top = '50%';
+					zoomedImage.style.left = '50%';
+					zoomedImage.style.transform = 'translate(-50%, -50%)';
+
+					setTimeout(function() {
+						resolve();
+					}, 1000);
 				});
 			}
 
 			function extendPX() {
 				return new Promise(function(resolve, reject) {
-					zoomedImage.css('width', '');
-					zoomedImage.css('height', '');
-					zoomedImage.animate({
-						'max-width': $('html').width() + 'px',
-						'max-height': $('html').height() + 'px',
-					}, 'linear', function() {
+					console.log("extendPX");
+					zoomedImage.style.width = '';
+					zoomedImage.style.height = '';
+
+					let htmlBounds = document.getElementsByTagName("html")[0].getBoundingClientRect();
+
+					zoomedImage.classList.add("maxWidthHeightTransition");
+					zoomedImage.style.maxWidth = htmlBounds.width + 'px';
+					zoomedImage.style.maxHeight = htmlBounds.height + 'px';
+
+					setTimeout(function() {
+						zoomedImage.classList.remove("maxWidthHeightTransition");
 						resolve();
-					});
+					}, 1000);
 				});
 			}
 
 			function extendPC() {
 				return new Promise(function(resolve, reject) {
-					zoomedImage[0].style.maxWidth = '';
-					zoomedImage[0].style.maxWidth = '100%';
-					zoomedImage[0].style.maxHeight = '';
-					zoomedImage[0].style.maxHeight = '100%';
-					resolve();
+					console.log("extendPC");
+					zoomedImage.style.maxWidth = '';
+					zoomedImage.style.maxWidth = '100%';
+					zoomedImage.style.maxHeight = '';
+					zoomedImage.style.maxHeight = '100%';
+
+					setTimeout(function() {
+						resolve();
+					}, 1000);
 				});
 			}
 
 			function setSource(thisEl, picture) {
 				return new Promise(function(resolve, reject) {
-					const source = $(picture).attr('data-source');
+					console.log("setSource");
+					const source = picture.getAttribute('data-source');
 					if (source) {
-						$(thisEl).find('.zoomed_image_source').text('Quelle: ' + source);
+						thisEl.querySelector('.zoomed_image_source').innerText = 'Quelle: ' + source;
 					} else {
-						$(thisEl).find('.zoomed_image_source').text('');
+						thisEl.querySelector('.zoomed_image_source').innerText = '';
 					}
-					resolve();
+
+					setTimeout(function() {
+						resolve();
+					}, 1000);
 				});
 			}
 
@@ -166,19 +198,19 @@ export default {
 			}
 		},
 		closeZoom() {
-			const zoomedImage = $(this.$el).find('.zoomed_image');
-			const zoomBlurBackground = $(this.$el).find('.zoom_blur_background');
+			const zoomedImage = this.$el.querySelector('.zoomed_image');
+			const zoomBlurBackground = this.$el.querySelector('.zoom_blur_background');
 
 			const self = this;
-			zoomBlurBackground.animate({
-				opacity: 0,
-			}, function() {
-				$(self.$el).removeClass('zoom');
-				zoomBlurBackground.hide();
-			});
+			zoomBlurBackground.style.opacity = 0;
 
-			zoomedImage.hide();
-			zoomedImage.removeAttr('style');
+			setTimeout(function() {
+				self.$el.classList.remove('zoom');
+				zoomBlurBackground.style.display = 'none';
+			}, 500);
+
+			zoomedImage.style.display = 'none';
+			zoomedImage.removeAttribute('style');
 			this.currentWebpSrc = '';
 			this.currentJpgSrc = '';
 		},
@@ -274,6 +306,8 @@ export default {
 		position: fixed;
 		width: 100%;
 		height: 100%;
+		opacity: 0;
+		transition: opacity 0.5s;
 	}
 	.gallery.zoom .zoom_blur_background {
 		z-index: 2000;
@@ -296,5 +330,11 @@ export default {
 	.zoomed_image {
 		position: fixed;
 		z-index: 2002;
+	}
+	.zoomed_image.topLeftTransition {
+		transition: left 1s linear, top 1s linear;
+	}
+	.zoomed_image.maxWidthHeightTransition {
+		transition: max-width 1s linear, max-height 1s linear;
 	}
 </style>
