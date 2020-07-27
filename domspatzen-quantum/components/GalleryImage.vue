4066d05d3e25e628aea531e1d2383e4f413759f5
Jonathan Treffler <mail@jonathan-treffler.de>
2020-07-27 16:21:21 +0200

    added image lazy loading

--- domspatzen-quantum/components/GalleryImage.vue
+++ domspatzen-quantum/components/GalleryImage.vue
@@ -10,7 +10,7 @@
 	>
 		<source :srcSet="require('~/assets/pictures/' + this.src + '?format=webp&resize&sizes[]=200&sizes[]=300&sizes[]=600&sizes[]=700').srcSet" type="image/webp">
 		<source :srcSet="require('~/assets/pictures/' + this.src + '?format=jpg&resize&sizes[]=200&sizes[]=300&sizes[]=600&sizes[]=700').srcSet" type="image/jpg">
-		<img :src="require('~/assets/pictures/' + src)">
+		<img :src="require('~/assets/pictures/' + src)" loading="lazy">
 	</picture>
 </template>
 <script>
